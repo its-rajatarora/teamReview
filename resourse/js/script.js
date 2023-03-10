8cc5720e10b3614f71dadf8c2f37a333caf724db
its-rajatarora <126852012+its-rajatarora@users.noreply.github.com>
2023-03-10 15:59:57 +0530

    file upload
    js resource

---
+++ resourse/js/script.js
@@ -0,0 +1,13 @@
+ $('.js--dhoni').click(function(){
+        $('html,body').animate({scrollTop: $('.js--c-dhoni').offset().top},1000);
+    })
+    $('.js--jadeja').click(function(){
+        $('html,body').animate({scrollTop: $('.js--vc-jadeja').offset().top},1000);
+    })
+
+ $('.js--team').click(function(){
+        $('html,body').animate({scrollTop: $('.js--p-team').offset().top},1000);
+    })
+    $('.js--ipl').click(function(){
+        $('html,body').animate({scrollTop: $('.js--c-ipl').offset().top},1000);
+    })
